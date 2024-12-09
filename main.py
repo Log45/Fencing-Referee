aa4828bbe36a0724e3fb9e7405ff45ef928a880f
Skylar Gallup <cwg7336@rit.edu>
2024-12-09 15:29:17 -0500

    Hooked up video stream to the scorebox detect-classify pipeline

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ Main program for processing videos and video streams
 '''
 
 import cv2
+import cv2_common
 from argparse import ArgumentParser, Namespace
+from yolo_scorebox_classifier import ScoreboxDetectorClassifier
 
+
+## Constants
+
+SCOREBOX_MODEL_PATH = 'trained_models/scorebox_detect/scorebox_detect.pt'
+
+
+## Code
 
 def get_stream(args: Namespace) -> cv2.VideoCapture:
     stream_method: str | None = args.mode
@@ -47,15 +56,30 @@ def main():
     parser.add_argument('-f', '--filename', required=False)
     args = parser.parse_args()
 
+    # Load model
+    scorebox_detector = ScoreboxDetectorClassifier(SCOREBOX_MODEL_PATH)
+
+    # Open video stream
     cap: cv2.VideoCapture = get_stream(args)
 
     while cap.isOpened():
+        # Get the next frame
         ret, frame = cap.read()
         if not ret:
             print('Stream ended; closing...')
             break
-        cv2.imshow('stream', frame)
-        cv2.waitKey(int(1000 / 30))
+
+        # Process
+        scorebox_classification, labeled_frame = scorebox_detector.detect_and_classify(frame, debug=True)
+        print(scorebox_classification)
+
+        # Display the frame and wait for 1/30th of a second
+        cv2.imshow('stream', labeled_frame)
+        # cv2.waitKey(int(1000 / 30))
+        if scorebox_classification != cv2_common.NO_SIDE:
+            cv2.waitKey(0)
+        else:
+            cv2.waitKey(1)
 
 
 if __name__ == "__main__":
